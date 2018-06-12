0d80c172795407c2a8d7a6eb6d5dd1500c2cbe61
huhongxiang <gohuhx@163.com>
2018-06-12 14:34:22 +0800

    commit sprintboot code

---
+++ src/main/java/com/thoughtwoks/nho30/Nho30Application.java
@@ -0,0 +1,12 @@
+package com.thoughtwoks.nho30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nho30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nho30Application.class, args);
+    }
+}
